258a941cac90f2547919c7f2407a4211156cf967
mari3233 <mari323@naver.com>
2022-09-22 12:13:05 +0900

    z

---
+++ 01_ottogi/js/script.js
@@ -0,0 +1,29 @@
+$(".tab li:eq(0) a").click(
+    function(){  
+        $(".tab a").removeClass("on");
+        $(this).addClass("on");
+
+        $(".tabcontent").hide();
+       // $(".tab1").show();  //display:block;
+       $(".tab1").css("display" , "flex");
+    }
+);
+$(".tab li:eq(1) a").click(
+    function(){  
+        $(".tab a").removeClass("on");
+        $(this).addClass("on");  
+
+        $(".tabcontent").hide();
+        $(".tab2").css("display" , "flex");
+    }
+);
+$(".tab li:eq(2) a").click(
+    function(){ 
+        $(".tab a").removeClass("on");
+        $(this).addClass("on");  
+
+        $(".tabcontent").hide();
+        $(".tab3").css("display" , "flex");
+    }
+);
+
---
+++ 보그반응형 - self/js/script.js
@@ -0,0 +1,6 @@
+$(".burger").click(
+    function(){
+        $(".pop_nav").toggleClass("on");
+        $(this).toggleClass("on");
+    }
+);
